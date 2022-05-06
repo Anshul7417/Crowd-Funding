41f7b96c0a0972a91185188208660ce37261488b
Anshul Tripathi <tripathianshul06@gmail.com>
2022-05-06 22:08:13 +0530

    voting functionality added

--- crowdfunding-sol.sol
+++ crowdfunding-sol.sol
@@ -72,4 +72,21 @@ contract crowdFunding{
         newRequest.noOfVoters=0;
     }
 
+    function voteRequest(uint _requestno) public{
+        require(contributors[msg.sender]>0,"You must be a contributor");
+        Request storage thisRequest=requests[_requestno];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyManager{
+    require(raisedAmount>=target);
+    Request storage thisRequest=requests[_requestNo];
+    require(thisRequest.completed==false, "The request has been completed");
+    require(thisRequest.noOfVoters > noOfContributors/2, "Majority does not support");
+    thisRequest.recipient.transfer(thisRequest.value);
+    thisRequest.completed=true;
+    }
+
 }
